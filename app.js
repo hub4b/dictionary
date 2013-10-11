3a35c1cdb54dc970a63f200136fe48a5f242f16f
Lauri Koutaniemi <lauri@koutaniemi.com>
2013-10-11 15:26:35 +0300

    proto

---
+++ app.js
@@ -0,0 +1,10 @@
+// check if mobile use and if appgyver is in use
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) device = true;
+if(typeof steroids === "undefined") appgyver = false;
+
+// init actions
+$(document).ready(function() {
+	
+	
+	
+});
