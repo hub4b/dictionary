c07f1a974f7206d4f0368d95478013f54ebb2725
Lauri Koutaniemi <lauri@koutaniemi.com>
2013-10-13 22:20:20 +0300

    fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,33 @@
+word = "";
 device = false;
+presscount = 0;
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) device = true;
 
-function search(term) {
-	$.ajax({
-		data: { term: term },
-		type: "POST", url: "http://www.koutaniemi.com/scripts/translate.php",
-		success: function(d) {
-			if(!d) $("#result").html("<center>ei tuloksia / ничего не найдено</center>");
-			else $("#result").html(d);
-		}
-	});
+function test(press) {
+	if(presscount==press) search();
+}
+
+function search() {
+	if(word) {
+		$("#result").html("<h2>Search results for '"+word+"'</h2><br>Loading, please wait...");
+		$.ajax({
+			data: { term: word },
+			type: "GET", url: "http://www.koutaniemi.com/scripts/translate.php",
+			success: function(d) {
+				if(!d) $("#result").html("<h2>Search results for '"+word+"'</h2><br>ei tuloksia / ничего не найдено");
+				else $("#result").html(d);
+			}
+		});
+	} else $("#result").html("<h2>Search results for '"+word+"'</h2><br>ei tuloksia / ничего не найдено");
 }
 
 $(document).ready(function() {
-	
-	$('#term').bind('keyup', function(e) { search($('#term').val()); });
-	
-	
+	$('#term').bind('keyup', function(e) {
+		if(word!=$('#term').val()) {
+			word = $('#term').val();
+			$("#result").html("<h2>Search results for '"+word+"'</h2><br>Loading, please wait...");
+			setTimeout(test,1000,++presscount);
+		}
+	});
 });
